94e62b3c9ef9933a84edfaab9496a5e441f20bf1
aldrut <a.drut@free.fr>
2021-10-11 15:14:05 +0200

    update api

--- api/controller/base.controller.php
+++ api/controller/base.controller.php
@@ -1,10 +1,78 @@
 <?php
 
+include_once 'DbManager.php';
+include_once 'Argon2.php';
+
+class BaseController
+{
+    function __contrust($routeParts)
+    {
+        $this->method = $_SERVER["REQUEST_METHOD"];
+
+        $firstParam = $routeParts[0] ?? null;
+        $id = isset($firstParam) ? intval($firstParam) : 0;
+
+        $this->body = $this->method == 'GET' ? $_GET : json_encode(file_get_contents('php://input'), true);
 
 
 
+        $this->action = null;
+        switch ($this->method) {
+            case 'GET':
+                $this->action = $id > 0 ? "getOne" : ($firstParam == "all" ? "getAll" : $firstParam);
+                break;
+
+            case 'POST':
+                $this->action = $firstParam != null ? $firstParam : "create";
+                break;
+
+            case 'PUT':
+                break;
+
+            case 'DELETE':
+                break;
+        
+        }
+
+        if(!isset($this->action) || !method_exists(get_called_class(),$this->action))
+        die(json_encode(false));
+
+        $this->table = $this->table ?? str_replace("Controller","",lcfirst(get_called_class()));
+
+        $this->connectBDD = new DbManager($this->table);
+        $this->jsonResponse = json_encode($this->{$this->action}($routeParts));
 
 
+    }
 
+    public function sendResponse()
+    {
+        echo $this->jsonResponse;
+    }
 
-?>
+    protected function getAll($params)
+    {
+        $id = array_shift($params);
+        $where ="";
+        foreach($params as $param)
+        {
+            $param = urlencode($param);
+            $where .= " $param AND";
+        }
+        $where = substr($where,0,-3);
+        if(!$where) $where =1;
+        return $this->connectBDD->getOne($id,$where);
+    }
+
+    protected function create(){
+        return "create an account $this->table";
+    }
+
+    protected function update(){
+        return "update an account $this->table";
+    }
+
+    protected function delete(){
+        return "delete an account $this->table";
+    }
+}
--- bdd/db_js.sql
+++ bdd/db_js.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3307
--- Généré le : lun. 11 oct. 2021 à 09:26
+-- Généré le : lun. 11 oct. 2021 à 12:49
 -- Version du serveur :  10.5.4-MariaDB
 -- Version de PHP : 7.4.9
 
@@ -47,6 +47,38 @@ CREATE TABLE IF NOT EXISTS `account` (
 
 INSERT INTO `account` (`id`, `firstname`, `lastname`, `email`, `password`, `token`, `active`, `deleted`, `updated_at`) VALUES
 (1, 'LAURENT', 'BEDU', 'lbedu@gmail.com', 'SzFaMjlsck41OUhqc203OA$6wwhzO7d2ql3YA0fQFQs9B9a4AIZ9cPb4B2qyTF6+IM', NULL, 1, 0, '2021-10-08 10:34:55');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `category`
+--
+
+INSERT INTO `category` (`id`, `name`, `description`, `deleted`, `updated_at`) VALUES
+(1, 'ante ipsum primis in faucibus', 'Morbi porttitor lorem id ligula.', 1, '2021-10-11 12:48:34'),
+(2, 'in ante vestibulum ante', 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 0, '2021-10-11 12:48:34'),
+(3, 'nullam varius nulla facilisi', 'Vivamus tortor. Duis mattis egestas metus. Aenean fermentum.', 0, '2021-10-11 12:48:34'),
+(4, 'viverra eget congue eget semper', 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', 1, '2021-10-11 12:48:34'),
+(5, 'aliquam sit amet diam in', 'Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', 0, '2021-10-11 12:48:34'),
+(6, 'sociis natoque penatibus et magnis', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 0, '2021-10-11 12:48:34'),
+(7, 'orci mauris lacinia', 'Nulla mollis molestie lorem.', 0, '2021-10-11 12:48:34'),
+(8, 'aliquam quis turpis eget elit', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 0, '2021-10-11 12:48:34'),
+(9, 'leo', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', 0, '2021-10-11 12:48:34'),
+(10, 'elementum eu interdum eu', 'Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia.', 1, '2021-10-11 12:48:34');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
